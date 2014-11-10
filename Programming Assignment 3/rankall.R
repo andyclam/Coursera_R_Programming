f62bd0fb4c4aa813d548b9cd11936c80a5dc2808
andyclam <chunyin91@gmail.com>
2014-11-09 19:47:33 -0800

    Course Completion

--- Programming Assignment 3/rankall.R
+++ Programming Assignment 3/rankall.R
@@ -12,61 +12,38 @@ rankall <- function(outcome, num = "best") {
       }
       
       ## For each state, find the hospital of the given rank
-      if(num == 'best'){
-            num <- 1
-      }
+#      if(num == 'best'){
+ #           num <- 1
+  #    }
 
+      
       
       outcome2 <- sub(" ", ".", outcome)
       searchterm <- paste("hospital.30.day.death..mortality..rates.from.", outcome2, sep = "")
       mortalityCol <- which(tolower(names(mydata)) == searchterm)
       
-      mydatasub <- as.data.frame(cbind('Hospital.Name' = mydata$Hospital.Name, 'State' = mydata$State, 'Mortality' = as.double(mydata[,mortalityCol])))
+      mydatasub <- data.frame(cbind('Hospital.Name' = mydata$Hospital.Name, 'State' = mydata$State, 'Mortality' = as.numeric(mydata[,mortalityCol])))
       mydatasub <- mydatasub[!is.na(mydatasub[,3]),]
-      mydatasub <- mydatasub[order(mydatasub[1]),]
       
-      mydatasplit <- split(mydatasub, mydatasub$State)
-      sorted <- lapply(mydatasplit, 
-                       function(x){
-                             x[order(x[1]),]
-                       }
-      )
-
-      ordered <- lapply(sorted, 
-                        function(x){
-                              order(x$Mortality)
-                        }
-      )
+      mydatasub <- mydatasub[order(mydatasub[,1]),]
+      mydatasub <- mydatasub[order(mydatasub[,3], decreasing = FALSE),]
       
-      
-      ranked <- data.frame()
-      num2 <- num
+      result <- data.frame()
       for(i in 1:length(states)){
-            if(num2 == 'worst'){
-                  num <- max(ordered[[i]])
+            sub <- mydatasub[which(mydatasub$State==states2[i]), ]
+            sub[,3] <- as.numeric(levels(sub[, 3]))[sub[, 3]]
+            sub <- sub[order(sub[,3]),]
+            values <- as.numeric(levels(sub[, 3]))[sub[, 3]]
+            if(num == "best"){
+                  rowNum <- which.min(values)
+            } else if(num == "worst"){
+                  rowNum <- which.max(values)
+            } else{
+                  rowNum <- num
             }
-            if(length(which(ordered[[i]] == num)) > 0){
-                  ranked <- rbind(ranked, sorted[[i]][which(ordered[[i]] == num),])
-            }else{
-                  ranked <- rbind(ranked, cbind('Hospital.Name' = '<NA>', 'State' = states2[i], 'Mortality' = 0))     
-            }
-            
-      }
-      
-      #ranked <- as.data.frame(ranked)
-      
-      
-      ## Return a data frame with the hospital names and the
-      ## (abbreviated) state name
-#      result <- data.frame()
- #     for(i in 1:length(states2)){
-  #          result <- data.frame(rbind(result, cbind('hospital' = ranked[[1]][which(states2[i] == ranked[2])], 'state' = states2[i])))
-#
- #     }
-      
-      result <- cbind('hospital' = ranked[1], 'state' = ranked[[2]])
-      names(result) <- c('hospital', 'state')
-      rownames(result) <- states2
+            result <- rbind(result, data.frame('hospital' = sub[rowNum, ]$Hospital.Name, 'state' = states2[i]))
+      }     
+
       result
       
 }
